5d73044fb6c7c3ac3e9f2c56d5dd40275aa60670
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-01 21:43:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,3 +4,10 @@ def square_array(array)
   end
 end
 
+def square_array(array)
+  counter = 0 
+  while counter < array.length do
+    array[counter] = array[counter] ** 2 
+    counter += 1 
+  end
+end
