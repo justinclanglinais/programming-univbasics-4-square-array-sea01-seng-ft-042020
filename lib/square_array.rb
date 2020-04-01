0db9d9453f22b779202f5b9a45bc6d8fdc0cacbe
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-01 21:48:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0 
   while counter < array.length do
-    array[counter] = (array[counter] ** 2)
+    new_var = array[counter]
+    sq_var = new_var ** 2 
+    array[counter] = sq_var
     counter += 1 
   end
 end
