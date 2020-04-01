33b8bc0f48325bb68a3be2ffbde7741bf2f64e38
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-01 21:37:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   length = array.length
-  length.times { | index |
-    array[index] = (array[index] ** 2)
-  }
+  length.times do |index|
+    array[index] = array[index ** 2]
+  end
 end
