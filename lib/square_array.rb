ea14ab92f5beec520480417221eda9b21535dc02
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-01 21:35:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  counter = 0
-  while counter < array.length do
-    array[counter] = array[counter].to_i ** 2
-    counter += 1
-  end
+  array.length.times { | index |
+    array[index] = array[index] ** 2
+  }
 end
